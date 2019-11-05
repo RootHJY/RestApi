ade19e235d8b11a556be9b5e2677b41a50c8ec9d
roothjy <roothjy@aliyun.com>
2019-11-05 22:28:36 +0800

    api

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 module.exports = {
     // 地址
-    address: 'localhost',
+    address: '127.0.0.1',
     // 端口
     port: '27017',
     // 数据库
     database: 'jiayan',
     // 用户名
-    username: 'jiayan',
+    username: '',
     // 密码
-    password: '123456'
+    password: ''
 }
